d5b2247c0dd00d9f5ba7f9ba2cafecf271e4b9a8
razin92 <s.razin92@gmail.com>
2018-03-05 10:36:17 +0500

    Revert "added info/forms"
    This reverts commit e211de01b822a9e4265224af57c16a6a07707e53.

--- sktv/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    subscriber_id = forms.CharField(label='Введите свой ID', max_length=9)
-    tel_number = forms.CharField(label='Введите свой номер мобильного телефона (с кодом)', max_length=13)
