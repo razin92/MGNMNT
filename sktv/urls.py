f1dab970be2fda9d579d930b5e9e929cb8a23de8
razin92 <s.razin92@gmail.com>
2018-07-23 12:58:55 +0500

    Bug fix connected with importing wrong module

--- sktv/urls.py
+++ sktv/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-from . import views_test
 
 app_name = 'sktv'
 
@@ -8,5 +7,7 @@ urlpatterns = [
     # Типы устройств
     url(r'device_type/$', views.DeviceTypeView.as_view(), name='device_type'),
     url(r'device_type/edit/(?P<pk>[0-9]+)/$', views.DeviceTypeEdit.as_view(), name='device_type_edit'),
-
+    # Модели устройств
+    url(r'device_model/$', views.DeviceModelView.as_view(), name='device_model'),
+    url(r'device_model/edit/(?P<pk>[0-9]+)/$', views.DeviceModelEdit.as_view(), name='device_model_edit'),
 ]
--- sktv/views.py
+++ sktv/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404, reverse, HttpResponseRed
 from django.views import View
 from django.urls import reverse_lazy
 from .models import DeviceType, DeviceModel, BuildingBase, Building, Device, BuildingDeviceGroup
-from .forms import DeviceTypeForm
+from .forms import DummyForm, DeviceTypeForm, DeviceModelForm
+from django.db.models import Model
 from django.contrib import messages
 from django.views.generic import UpdateView
 from django.core.paginator import Paginator
@@ -16,19 +17,32 @@ def get_requested_url(request):
     return url
 
 
-class DeviceTypeView(View):
+class DummyModel(Model):
     """
-    List of devices and creating form
+    The class for dynamic configuration of a view
     """
-    form_class = DeviceTypeForm
-    template = 'sktv/device_type.html'
+    pass
+
+
+class BasedView(View):
+    """
+    Based View class for all models
+    """
+    template = 'sktv/shared_template.html'
+    based_url = ''
+    form_class = DummyForm
+    model_class = DummyModel
+    ordering = ''
+    exclude_list = []
 
     def get(self, request):
         form = self.form_class(None)
-        devices = DeviceType.objects.all().order_by('name')
+        data = self.model_class.objects.all().order_by('%s' % self.ordering)
+        headers = [x for x in self.model_class._meta.get_fields() if x.name not in self.exclude_list]
         context = {
             'form': form,
-            'devices': devices
+            'data': data,
+            'headers': headers
         }
         return render(request, self.template, context)
 
@@ -36,11 +50,111 @@ class DeviceTypeView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('sktv:device_type'))
+            return HttpResponseRedirect(reverse('%s' % self.based_url))
         return render(request, self.template, context={'form': form})
+
+
+# Device Types
+class DeviceTypeView(BasedView):
+    """
+    List of type of devices and creating form
+    """
+    # template = 'sktv/device_type.html'
+    based_url = 'sktv:device_type'
+    form_class = DeviceTypeForm
+    model_class = DeviceType
+    ordering = 'name'
+    exclude_list = ['devicemodel']
+
 
 class DeviceTypeEdit(UpdateView):
     model = DeviceType
     fields = ['name']
     template_name = 'sktv/edit_form.html'
     success_url = reverse_lazy('sktv:device_type')
+
+
+# Device Models
+class DeviceModelView(BasedView):
+    """
+    List of model of devices and creating form
+    """
+    #template = 'sktv/device_model.html'
+    based_url = 'sktv:device_model'
+    form_class = DeviceModelForm
+    model_class = DeviceModel
+    ordering = 'id'
+    exclude_list = ['device']
+
+
+class DeviceModelEdit(UpdateView):
+    model = DeviceModel
+    fields = ['device_type', 'model']
+    template_name = 'sktv/edit_form.html'
+    success_url = reverse_lazy('sktv:device_model')
+
+
+# Building Base
+class BuildingBaseView(View):
+    template = ''
+    based_url = ''
+    form_class = DummyForm
+    model_class = DummyModel
+    ordering = ''
+
+
+class BuildingBaseEdit(View):
+    model = DeviceModel
+    fields = ['device_type', 'model']
+    template_name = 'sktv/edit_form.html'
+    success_url = reverse_lazy('sktv:device_model')
+
+
+# Building
+class BuildingView(View):
+    template = ''
+    based_url = ''
+    form_class = DummyForm
+    model_class = DummyModel
+    ordering = ''
+
+
+class BuildingEdit(View):
+    model = DeviceModel
+    fields = ['device_type', 'model']
+    template_name = 'sktv/edit_form.html'
+    success_url = reverse_lazy('sktv:device_model')
+
+
+# Device
+class DeviceView(View):
+    template = ''
+    based_url = ''
+    form_class = DummyForm
+    model_class = DummyModel
+    ordering = ''
+
+
+class DeviceEdit(View):
+    model = DeviceModel
+    fields = ['device_type', 'model']
+    template_name = 'sktv/edit_form.html'
+    success_url = reverse_lazy('sktv:device_model')
+
+
+# BuildingDeviceGroup
+class BuildingDeviceGroupView(View):
+    template = ''
+    based_url = ''
+    form_class = DummyForm
+    model_class = DummyModel
+    ordering = ''
+
+
+class BuildingDeviceGroupEdit(View):
+    model = DeviceModel
+    fields = ['device_type', 'model']
+    template_name = 'sktv/edit_form.html'
+    success_url = reverse_lazy('sktv:device_model')
+
+
